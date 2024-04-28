5207e6f46aee2e93fbfa65ecf8e7aac06a056c8a
saurabh kumar <71440536+saurabh-kud@users.noreply.github.com>
2024-04-28 13:35:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,28 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+function checkOrigin(req, res, next) {
+  const allowedOrigins = [
+    "http://localhost:5173"
+  ];
+
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+  } 
+next();
+  
+}
+
+app.use(checkOrigin);
+
+
 //user api route
 app.use("/v1/api/user", require("./Routes/UserRoute/userRoute"));
 app.use("/v1/api", require("./Routes/UserAuth/userAuth"));
